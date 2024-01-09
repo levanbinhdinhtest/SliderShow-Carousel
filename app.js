fafb8d8df3f26dd00a8bec5814d44d1c3699d9c0
Le Van Binh Dinh <biskull03@gmail.com>
2024-01-09 23:08:13 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+let listImg = document.querySelectorAll('.list-image div img')
+console.log(listImg)
+let btnNext = document.querySelector('.control-next')
+let btnPrev = document.querySelector('.control-prev')
+let imageShow = document.querySelector('.main-image img')
+let itemImage = document.querySelectorAll('.list-image div')
+console.log(itemImage)
+
+
+//lay ra vi tri cua anh
+let currenIndex = 0
+// function borderChoose(currenIndex){
+//     itemImage.forEach(item=>{
+//         item.classList.remove('active')
+//     })
+
+//     listImg[currenIndex].parentElement.classList.add('active')
+// }
+
+listImg.forEach(function(item,index){
+    item.addEventListener('click',function(){
+        currenIndex = index
+        slideImageShow()
+    })
+})
+function slideImageShow(){
+    imageShow.src = listImg[currenIndex].src;
+}
+
+btnNext.addEventListener('click',function(){
+    if(currenIndex<listImg.length-1){
+        currenIndex++
+        slideImageShow()
+        console.log(currenIndex)
+    }
+    else{
+        currenIndex = 0
+        slideImageShow()
+    }
+      
+})
+btnPrev.addEventListener('click',function(){
+    if(currenIndex>0){
+        currenIndex--
+        slideImageShow()
+        console.log(currenIndex)
+    }
+    else{
+        currenIndex = listImg.length-1
+        slideImageShow()
+    }
+})
